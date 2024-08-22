9f92ced66e5cbe336b2b93a103a74bb5f9e751a3
Florian Carrier <Akaizoku@users.noreply.github.com>
2024-08-22 17:42:02 +0200

    Add license API support

---
+++ Public/API/License/Get-LicensedProducts.ps1
@@ -0,0 +1,44 @@
+function Get-LicensedProducts {
+    <#
+        .SYNOPSIS
+        Fetch list of licensed products
+
+        .DESCRIPTION
+        Retrieves the list of products associated with a specified account from Alteryx License Portal
+
+        .NOTES
+        File name:      Get-LicensedProducts.ps1
+        Author:         Florian Carrier
+        Creation date:  2024-08-22
+        Last modified:  2024-08-22
+    #>
+    [CmdletBinding ()]
+    Param (
+        [Parameter (
+            Position    = 1,
+            Mandatory   = $true,
+            HelpMessage = "Alteryx Account ID"
+        )]
+        [ValidateNotNullOrEmpty ()]
+        [System.String]
+        [Alias ("Account")]
+        $AccountID,
+        [Parameter (
+            Position    = 2,
+            Mandatory   = $true,
+            HelpMessage = "Access token"
+        )]
+        [ValidateNotNullOrEmpty ()]
+        [System.String]
+        [Alias ("AccessToken")]
+        $Token
+    )
+    Begin {
+        # Get global preference vrariables
+        Get-CallerPreference -Cmdlet $PSCmdlet -SessionState $ExecutionContext.SessionState
+    }
+    Process {
+        $Products = Invoke-AlteryxLicenseAPI -Token $AccessToken -Endpoint "v1/products" -AccountID $AccountID
+        return ($Products | ConvertFrom-Json)
+    }
+}
