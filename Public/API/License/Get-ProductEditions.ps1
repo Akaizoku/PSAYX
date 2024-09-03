5f0178cb8c03dc891aabe9a8dcc76a789e691f60
Florian Carrier <Akaizoku@users.noreply.github.com>
2024-09-03 19:28:18 +0200

    Create Get-ProductEditions.ps1
    Add Get-ProductEditions

---
+++ Public/API/License/Get-ProductEditions.ps1
@@ -0,0 +1,61 @@
+function Get-ProductEditions {
+    <#
+        .SYNOPSIS
+        Find all download editions for the provided product line's release.
+
+        .DESCRIPTION
+        Find all download editions for the provided product line's release that the current authenticated user can access under the account whose ID is accountId. 
+        
+        .NOTES
+        File name:      Get-ProductEditions.ps1
+        Author:         Florian Carrier
+        Creation date:  2024-09-03
+        Last modified:  2024-09-03
+    #>
+    [CmdletBinding ()]
+    Param (
+        [Parameter (
+            Position    = 1,
+            Mandatory   = $true,
+            HelpMessage = "Alteryx Account ID"
+        )]
+        [ValidateNotNullOrEmpty ()]
+        [System.String]
+        [Alias ("Account")]
+        $AccountID,
+        [Parameter (
+            Position    = 2,
+            Mandatory   = $true,
+            HelpMessage = "Access token"
+        )]
+        [ValidateNotNullOrEmpty ()]
+        [System.String]
+        [Alias ("AccessToken")]
+        $Token,
+        [Parameter (
+            Position    = 3,
+            Mandatory   = $true,
+            HelpMessage = "Alteryx product release ID"
+        )]
+        [ValidateNotNullOrEmpty ()]
+        [System.String]
+        [Alias ("Release", "productLineReleaseId")]
+        $ReleaseID
+    )
+    Begin {
+        # Get global preference vrariables
+        Get-CallerPreference -Cmdlet $PSCmdlet -SessionState $ExecutionContext.SessionState
+        # Define parameters
+        $Parameters = [Ordered]@{
+            "productLineReleaseId" = $ReleaseID
+        }
+    }
+    Process {
+        # Fetch list of releases
+        $Editions = Invoke-AlteryxLicenseAPI -Token $AccessToken -Endpoint "v1/products/-/releases/-/editions" -AccountID $AccountID -Parameters $Parameters
+        # Sort editions in alphabetic order
+        $SortedEditions = ($Editions | ConvertFrom-Json) | Sort-Object -Property "description"
+        # Return list of editions
+        return $SortedEditions
+    }
+}
